d30d598053c9a0577105e5911175e584d5d304ce
Rusty Shackleford <a@neb.best>
2021-01-07 02:58:51 -0500

    initial

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin2.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin2.java
@@ -1,3 +1,4 @@
+
 package net.fabricmc.example.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
@@ -8,10 +9,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!!!");
+@Mixin(ClientPlayerEntity.class)
+public class ExampleMixin2 {
+	@Inject(at = @At("HEAD"), method= "tick()V")
+	public void tick(CallbackInfo info) {
+		System.out.println("TICK!!");
 	}
 }
